13ce53ab2870e4ae1af3325f78a3c56f35cbc8b4
Anatolii Petrov <hello@aswitch.me>
2024-03-19 18:40:04 +0100

    Initial Commit

---
+++ POPatterns/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  POPatterns
+//
+//  Created by Anatoliy Petrov on 19.3.24..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ POPatterns/POPatternsApp.swift
@@ -0,0 +1,17 @@
+//
+//  POPatternsApp.swift
+//  POPatterns
+//
+//  Created by Anatoliy Petrov on 19.3.24..
+//
+
+import SwiftUI
+
+@main
+struct POPatternsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
